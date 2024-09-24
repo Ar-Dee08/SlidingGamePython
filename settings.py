b7c35d0e5daf93c6adfc7b2c6a13a03c0c599b8f
RD <147609266+Ar-Dee08@users.noreply.github.com>
2024-09-24 15:39:08 +0800

    Add files via upload
    Install Pygames (pip install pygame)

---
+++ settings.py
@@ -0,0 +1,15 @@
+# COLORS (r, g, b)
+LIGHTGREEN = (189, 232, 202)
+BLACK = (0, 0, 0)
+GREEN = (13, 124, 102)
+LIGHTGREY = (100, 100, 100)
+BGCOLOUR = GREEN
+GREEN = (65, 179, 162)
+
+# game settings
+WIDTH = 800
+HEIGHT = 500
+FPS = 60
+title = "Sliding Puzzle Game"
+TILESIZE = 128
+GAME_SIZE = 3
